ba2d55108367d0973fe77a959204d5b24ff38b8f
Otto Ringhofer <otto.ringhofer@gmail.com>
2016-05-19 21:21:33 +0200

    implement session guardian
      is supervisor for all session actors
      VaactorSession implements session, abstract initialSession for init value
      akka 2.4.5

--- addon/src/main/scala/org/vaadin/addons/vaactor/VaactorSession.scala
+++ addon/src/main/scala/org/vaadin/addons/vaactor/VaactorSession.scala
@@ -2,11 +2,26 @@ package org.vaadin.addons.vaactor
 
 import VaactorSession._
 
-import akka.actor.{ Actor, ActorRef }
+import akka.actor.{ Actor, ActorRef, Props }
 
 import scala.collection.mutable
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 object VaactorSession {
+
+  val sessionConfig = vaactorConfig.getConfig("session")
+
+  class Guardian extends Actor {
+    private var sessions: Int = 0
+
+    def receive = {
+      case props: Props =>
+        sessions += 1
+        val name = s"${ self.path.name }-${ props.actorClass.getSimpleName }-$sessions"
+        sender ! context.actorOf(props, name) // neuen Kind-Actor erzeugen
+    }
+  }
 
   /** send current session to sender */
   case object RequestSession
@@ -20,6 +35,18 @@ object VaactorSession {
   /** remove sender from uiActorSet */
   case object UnsubscribeUI
 
+  val guardian = VaactorServlet.system.actorOf(
+    Props[Guardian], sessionConfig.getString("guardian-name"))
+
+  import akka.pattern.ask
+  import akka.util.Timeout
+
+  private val askTimeout = Timeout(sessionConfig.getInt("ask-timeout").seconds)
+
+  def actorOf(props: Props): ActorRef = {
+    Await.result((guardian ? props) (askTimeout).mapTo[ActorRef], Duration.Inf)
+  }
+
 }
 
 trait VaactorSession[S] {
@@ -27,8 +54,16 @@ trait VaactorSession[S] {
 
   private[vaactor] val uiActors = mutable.Set.empty[ActorRef]
 
+  private var _session = initialSession
+
+  /** returns initial value of session */
+  def initialSession: S
+
   /** returns current session */
-  def session: S
+  def session: S = _session
+
+  /** sets current session */
+  def session_=(s: S): Unit = _session = s
 
   /** behaviour handles all messages defined in [[VaactorSession]] */
   val sessionBehaviour: Receive = {
--- addon/src/test/scala/org/vaadin/addons/vaactor/VaactorSessionSpec.scala
+++ addon/src/test/scala/org/vaadin/addons/vaactor/VaactorSessionSpec.scala
@@ -1,37 +1,51 @@
 package org.vaadin.addons.vaactor
 
 import VaactorSessionSpec._
-import org.vaadin.addons.vaactor.VaactorSession.{ BroadcastSession, RequestSession, SubscribeUI, UnsubscribeUI }
+import org.vaadin.addons.vaactor.VaactorSession._
 
-import akka.actor.Actor
+import akka.actor.{ Actor, Props }
 import akka.testkit.TestActorRef
 
 object VaactorSessionSpec {
 
   case class TestSession(state: String)
 
+  case object Crash
+
   val defaultSessionState = "$test$Session$"
   val DefaultSession = TestSession(defaultSessionState)
   val EmptySession = TestSession("")
 
-  class SessionBehaviourActor extends Actor with VaactorSession[TestSession] {
-    val session = DefaultSession
+  class TestActor extends Actor with VaactorSession[TestSession] {
+    def initialSession = DefaultSession
 
-    val receive = sessionBehaviour
+    val testBehaviour: Receive = {
+      case s: TestSession => session = s
+      case Crash => throw new Exception("Crash received")
+    }
+    val receive = sessionBehaviour orElse testBehaviour
   }
 
+  val testProps = Props[TestActor]
 }
 
 class VaactorSessionSpec extends AkkaSpec {
 
+  "VaactorSession.session" should "set and return session" in {
+    val actor = TestActorRef[TestActor]
+    actor.underlyingActor.session shouldBe DefaultSession
+    actor.underlyingActor.session = EmptySession
+    actor.underlyingActor.session shouldBe EmptySession
+  }
+
   "VaactorSession.sessionBehaviour" should "reply session to sender on RequestSession" in {
-    val actor = TestActorRef[SessionBehaviourActor]
+    val actor = TestActorRef[TestActor]
     actor ! RequestSession
     expectMsgType[TestSession](waittime) shouldBe DefaultSession
   }
 
   it should "manage sender in uiActors on SubscribeUI and UnsubscribeUI" in {
-    val actor = TestActorRef[SessionBehaviourActor]
+    val actor = TestActorRef[TestActor]
     actor.underlyingActor.uiActors.size shouldBe 0 // initially empty
     actor ! SubscribeUI
     actor.underlyingActor.uiActors.size shouldBe 1 // sender added
@@ -42,17 +56,47 @@ class VaactorSessionSpec extends AkkaSpec {
   }
 
   it should "broadcast session on BroadcastSession" in {
-    val actor = TestActorRef[SessionBehaviourActor]
+    val actor = TestActorRef[TestActor]
     actor ! SubscribeUI
     actor ! BroadcastSession
     expectMsgType[TestSession](waittime) shouldBe DefaultSession
   }
 
+  it should "set session on receive of Session" in {
+    val actor = TestActorRef[TestActor]
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe DefaultSession
+    actor ! EmptySession
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe EmptySession
+  }
+
   "VaactorSession.broadcast" should "broadcast message to all registered Uis" in {
-    val actor = TestActorRef[SessionBehaviourActor]
+    val actor = TestActorRef[TestActor]
     actor ! SubscribeUI
     actor.underlyingActor.broadcast(defaultSessionState)
     expectMsgType[String](waittime) shouldBe defaultSessionState
+  }
+
+  "VaadinSession.actorOf" should "return restarting ActorRef" in {
+    val actor = VaactorSession.actorOf(testProps)
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe DefaultSession
+    actor ! Crash
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe DefaultSession
+  }
+
+  it should "kepp session when restarted" in {
+    val actor = VaactorSession.actorOf(testProps)
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe DefaultSession
+    actor ! EmptySession
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe EmptySession
+    // actor ! Crash
+    actor ! RequestSession
+    expectMsgType[TestSession](waittime) shouldBe EmptySession
   }
 
 }
