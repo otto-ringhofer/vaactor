fe70270cb610e3f44b6698610d150ff574d7fffb
Otto Ringhofer <otto.ringhofer@gmail.com>
2017-10-04 08:10:00 +0200

    replace deprecated expectNoMsg()

--- test/src/test/scala/org/vaadin/addons/vaactor/AkkaSpec.scala
+++ test/src/test/scala/org/vaadin/addons/vaactor/AkkaSpec.scala
@@ -4,8 +4,12 @@ import org.scalatest.{ BeforeAndAfterAll, FreeSpecLike, Inside, Matchers }
 
 import akka.testkit.{ DefaultTimeout, ImplicitSender, TestKit }
 
+import scala.concurrent.duration._
+
 abstract class AkkaSpec extends TestKit(VaactorServlet.system)
   with FreeSpecLike with BeforeAndAfterAll with Matchers with Inside with DefaultTimeout with ImplicitSender {
+
+  val noMessageWait: FiniteDuration = 1.seconds
 
   override def afterAll: Unit = {
     super.afterAll
