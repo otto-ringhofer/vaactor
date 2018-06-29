df0c0a82e28b9c94c0e78cd5f9be277343072d63
Otto Ringhofer <otto.ringhofer@gmail.com>
2018-06-29 12:48:54 +0200

    cleanup Example code

--- example/src/main/scala/org/vaadin/addons/vaactor/example/Example.scala
+++ example/src/main/scala/org/vaadin/addons/vaactor/example/Example.scala
@@ -7,7 +7,7 @@ import org.vaadin.addons.vaactor._
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.Label
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
-import com.vaadin.flow.component.page.{ BodySize, Push }
+import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.server.VaadinServletConfiguration
 import com.vaadin.flow.shared.communication.PushMode
@@ -32,26 +32,17 @@ object ExampleObject {
 
 }
 
-@WebServlet(
-  urlPatterns = Array("/*"),
-  asyncSupported = true
-)
-@VaadinServletConfiguration(
-  productionMode = false
-)
+@WebServlet(urlPatterns = Array("/*"), asyncSupported = true)
+@VaadinServletConfiguration(productionMode = false)
 class ExampleServlet extends VaactorSessionServlet {
 
   override val sessionProps: Props = Props(classOf[ExampleSessionActor])
 
 }
 
-@BodySize(height = "100vh", width = "100vw")
 @Route("")
-@Theme(classOf[Lumo])
-@Push(
-  value = PushMode.AUTOMATIC,
-  transport = Transport.WEBSOCKET
-)
+@Theme(value = classOf[Lumo], variant = Lumo.DARK)
+@Push(value = PushMode.AUTOMATIC, transport = Transport.WEBSOCKET)
 class ExampleUI extends VerticalLayout with Vaactor.HasActor with Vaactor.HasSession {
 
   // counter local to this UI
@@ -61,16 +52,23 @@ class ExampleUI extends VerticalLayout with Vaactor.HasActor with Vaactor.HasSes
 
   setMargin(true)
   setSpacing(true)
-  add(new Label("Vaactor Example") {
-    // todo      addStyleName(ValoTheme.LABEL_H1)
-  })
-  add(new Button("Click Me", { _ =>
-    uiCnt += 1
-    session ! s"Thanks for clicking! (uiCnt:$uiCnt)"
-  })
+  add(new Label("Vaactor Example"))
+  add(
+    new Button("Click Me", { _ =>
+      uiCnt += 1
+      session ! s"Thanks for clicking! (uiCnt:$uiCnt)"
+    })
   )
   add(stateDisplay)
-  add(new ExampleStateButton(this))
+  add(
+    new Button("Show Counts") with Vaactor.HasActor {
+      addClickListener(_ => session ! VaactorSession.RequestSessionState)
+
+      override def receive: Receive = {
+        case state: Int => setText(s"Show Counts - uiCnt is $uiCnt, sessionCnt is $state, globalCnt is $globalCnt")
+      }
+    }
+  )
 
   override def receive: Receive = {
     case hello: String =>
@@ -88,18 +86,6 @@ class ExampleSessionActor extends Actor with VaactorSession[Int] {
     case msg: String =>
       sessionState += 1
       sender ! s"$msg (sessionCnt:$sessionState)"
-  }
-
-}
-
-class ExampleStateButton(val hasSession: Vaactor.HasSession)
-  extends Button with Vaactor.HasActor {
-
-  setText("SessionState")
-  addClickListener { _ => hasSession.session ! VaactorSession.RequestSessionState }
-
-  override def receive: Receive = {
-    case state: Int => setText(s"SessionState is $state")
   }
 
 }
